accbad779d91c6ca940b5199d9d7e3ad1b816421
Sebastiano Merlino <etr@zencoders.org>
2013-01-20 12:35:12 +0100

    Changed code in examples to compile with the new interface

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -25,7 +25,7 @@ Test2::Test2() : http_resource()
 {
 }
 
-http_response Test::render_GET(const http_request& r)
+void Test::render_GET(const http_request& r, http_response** res)
 {
 /*	cout << r.get_version() << endl;
 	cout << r.get_requestor() << endl;
@@ -43,10 +43,10 @@ http_response Test::render_GET(const http_request& r)
     topics.push_back("prova");
     //return long_polling_receive_response("", 200, "", topics, 10, "keepalive\n");
     cout << r.get_querystring() << endl;
-    return http_file_response("/home/etr/progs/libhttpserver/test/noimg.png", 200, "image/png");
+    *res = new http_file_response("/home/etr/progs/libhttpserver/test/noimg.png", 200, "image/png");
 }
 
-http_response Test::render_POST(const http_request& r)
+void Test::render_POST(const http_request& r, http_response** res)
 {
 /*	fstream filestr;
 	filestr.open("test.txt", fstream::out | fstream::app);
@@ -59,16 +59,16 @@ http_response Test::render_POST(const http_request& r)
         cout << vv[i] << endl;
     }
 	return http_string_response("OK",200);*/
-	http_string_response s("OK",100);
-    s.set_header(http_utils::http_header_location, "B");
-    s.set_cookie("Ciccio", "Puppo");
-    s.set_cookie("Peppe", "Puppo");
-    cout << s.get_cookie("Ciccio") << endl;
-    return s;
+	http_string_response* s = new http_string_response("OK",100);
+    s->set_header(http_utils::http_header_location, "B");
+    s->set_cookie("Ciccio", "Puppo");
+    s->set_cookie("Peppe", "Puppo");
+    cout << s->get_cookie("Ciccio") << endl;
+    *res = s;
  //   return long_polling_send_response("<script type=\"text/javascript\">alert(\"ciao\")</script>\n", "prova");
 }
 
-http_response Test2::render_GET(const http_request& r)
+void Test2::render_GET(const http_request& r, http_response** res)
 {
 	cout << "D2" << endl;
     typedef std::map<std::string, std::string,
@@ -77,18 +77,18 @@ http_response Test2::render_GET(const http_request& r)
     r.get_cookies(c);
     for(c_type::const_iterator it = c.begin(); it != c.end(); ++it)
         cout << (*it).first << " -> " << (*it).second << endl;
-	return http_string_response("{\" var1 \" : \" "+r.get_arg("var1")+" \", \" var2 \" : \" "+r.get_arg("var2")+" \", \" var3 \" : \" "+r.get_arg("var3")+" \"}", 200);
+	*res = new http_string_response("{\" var1 \" : \" "+r.get_arg("var1")+" \", \" var2 \" : \" "+r.get_arg("var2")+" \", \" var3 \" : \" "+r.get_arg("var3")+" \"}", 200);
 }
 
-http_response Test::render_PUT(const http_request& r)
+void Test::render_PUT(const http_request& r, http_response** res)
 {
-	return http_string_response(r.get_content(), 200);
+	*res = new http_string_response(r.get_content(), 200);
 }
 
 int main()
 {
 //    signal(SIGINT, &signal_callback_handler);
-	webserver ws = create_webserver(8080).start_method(http_utils::INTERNAL_REMANAGED).max_threads(5);
+	webserver ws = create_webserver(8080).max_threads(5);
     ws_ptr = &ws;
 	Test dt = Test();
 	Test2 dt2 = Test2();
--- examples/Test.hpp
+++ examples/Test.hpp
@@ -4,18 +4,18 @@
 
 using namespace httpserver;
 
-class Test : virtual public http_resource {
+class Test : public http_resource<Test> {
 	public:
         Test();
-		virtual http_response render_GET(const http_request&);
-        virtual http_response render_PUT(const http_request&);
-        virtual http_response render_POST(const http_request&);
+		void render_GET(const http_request&, http_response**);
+        void render_PUT(const http_request&, http_response**);
+        void render_POST(const http_request&, http_response**);
 };
 
-class Test2 : virtual public http_resource {
+class Test2 : public http_resource<Test2> {
 	public:
         Test2();
-        virtual http_response render_GET(const http_request&);
+        void render_GET(const http_request&, http_response**);
 };
 
 #endif
