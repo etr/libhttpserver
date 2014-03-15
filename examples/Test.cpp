8a14fd1126cef567c45c90289809fd7fbd7b89cd
Sebastiano Merlino <etr@zencoders.org>
2014-03-15 12:50:14 +0000

    Added license to files missing it

--- examples/Test.cpp
+++ examples/Test.cpp
@@ -1,3 +1,23 @@
+/*
+     This file is part of libhttpserver
+     Copyright (C) 2011, 2012, 2013, 2014 Sebastiano Merlino
+
+     This library is free software; you can redistribute it and/or
+     modify it under the terms of the GNU Lesser General Public
+     License as published by the Free Software Foundation; either
+     version 2.1 of the License, or (at your option) any later version.
+
+     This library is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Lesser General Public License for more details.
+
+     You should have received a copy of the GNU Lesser General Public
+     License along with this library; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+     USA
+*/
+
 #include "Test.hpp"
 #include <signal.h>
 #include <string>
@@ -17,11 +37,11 @@ void signal_callback_handler(int signum)
     ws_ptr->stop();
 }
 */
-Test::Test() : http_resource() 
+Test::Test() : http_resource()
 {
 }
 
-Test2::Test2() : http_resource() 
+Test2::Test2() : http_resource()
 {
 }
 
