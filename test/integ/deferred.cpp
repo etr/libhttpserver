cbb8d889660da50a36b0a6fa6c6b04e6c185a4fe
Sebastiano Merlino <electrictwister2000@gmail.com>
2019-01-04 21:36:57 +0000

    Added tests on deferred resources

---
+++ test/integ/deferred.cpp
@@ -0,0 +1,110 @@
+/*
+     This file is part of libhttpserver
+     Copyright (C) 2011-2019 Sebastiano Merlino
+
+     This library is free software; you can redistribute it and/or
+     modify it under the terms of the GNU Lesser General Public
+     License as published by the Free Software Foundation; either
+     version 2.1 of the License, or (at your option) any later version.
+
+     This library is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Lesser General Public License for more details.
+
+     You should have received a copy of the GNU Lesser General Public
+     License along with this library; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+     USA
+*/
+
+#if defined(__MINGW32__) || defined(__CYGWIN32__)
+#define _WINDOWS
+#undef _WIN32_WINNT
+#define _WIN32_WINNT 0x600
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#else
+#include <arpa/inet.h>
+#endif
+
+#include "littletest.hpp"
+#include <curl/curl.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
+#include "httpserver.hpp"
+#include <unistd.h>
+#include <signal.h>
+
+using namespace std;
+using namespace httpserver;
+
+size_t writefunc(void *ptr, size_t size, size_t nmemb, std::string *s)
+{
+    s->append((char*) ptr, size*nmemb);
+    return size*nmemb;
+}
+
+static int counter = 0;
+
+ssize_t test_callback (char* buf, size_t max)
+{
+    if (counter == 2)
+    {
+        return -1;
+    }
+    else
+    {
+        memset(buf, 0, max);
+        strcat(buf, "test");
+        counter++;
+        return strlen(buf);
+    }
+}
+
+class deferred_resource : public httpserver::http_resource
+{
+    public:
+        const httpserver::http_response render_GET(const httpserver::http_request& req)
+        {
+            return httpserver::http_response_builder("cycle callback response").deferred_response(test_callback);
+        }
+};
+
+LT_BEGIN_SUITE(deferred_suite)
+    webserver* ws;
+
+    void set_up()
+    {
+        ws = new webserver(create_webserver(8080));
+        ws->start(false);
+    }
+
+    void tear_down()
+    {
+        ws->stop();
+        delete ws;
+    }
+LT_END_SUITE(deferred_suite)
+
+LT_BEGIN_AUTO_TEST(deferred_suite, deferred_response)
+    deferred_resource* resource = new deferred_resource();
+    ws->register_resource("base", resource);
+    curl_global_init(CURL_GLOBAL_ALL);
+
+    std::string s;
+    CURL *curl = curl_easy_init();
+    CURLcode res;
+    curl_easy_setopt(curl, CURLOPT_URL, "localhost:8080/base");
+    curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
+    curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, writefunc);
+    curl_easy_setopt(curl, CURLOPT_WRITEDATA, &s);
+    res = curl_easy_perform(curl);
+    LT_ASSERT_EQ(res, 0);
+    LT_CHECK_EQ(s, "testtest");
+    curl_easy_cleanup(curl);
+LT_END_AUTO_TEST(deferred_response)
+
+LT_BEGIN_AUTO_TEST_ENV()
+    AUTORUN_TESTS()
+LT_END_AUTO_TEST_ENV()
