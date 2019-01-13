cd75f74dacacc87989ebdbb8c0f46e904d2b5df1
Sebastiano Merlino <electrictwister2000@gmail.com>
2019-01-13 21:53:56 +0000

    Additional examples
    Digest authentication.
    Deferred response.
    File response.
    HTTPS.

---
+++ examples/digest_authentication.cpp
@@ -0,0 +1,51 @@
+/*
+     This file is part of libhttpserver
+     Copyright (C) 2011, 2012, 2013, 2014, 2015 Sebastiano Merlino
+
+     This library is free software; you can redistribute it and/or
+     modify it under the terms of the GNU Lesser General Public
+     License as published by the Free Software Foundation; either
+     version 2.1 of the License, or (at your option) any later version.
+
+     This library is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Lesser General Public License for more details.
+
+     You should have received a copy of the GNU Lesser General Public
+     License along with this library; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+     USA
+*/
+
+#include <httpserver.hpp>
+
+#define MY_OPAQUE "11733b200778ce33060f31c9af70a870ba96ddd4"
+
+using namespace httpserver;
+
+class digest_resource : public httpserver::http_resource {
+public:
+    const std::shared_ptr<http_response> render_GET(const http_request& req) {
+        if (req.get_digested_user() == "") {
+            return std::shared_ptr<digest_auth_fail_response>(new digest_auth_fail_response("FAIL", "test@example.com", MY_OPAQUE, true));
+        }
+        else {
+            bool reload_nonce = false;
+            if(!req.check_digest_auth("test@example.com", "mypass", 300, reload_nonce)) {
+                return std::shared_ptr<digest_auth_fail_response>(new digest_auth_fail_response("FAIL", "test@example.com", MY_OPAQUE, reload_nonce));
+            }
+        }
+        return std::shared_ptr<string_response>(new string_response("SUCCESS", 200, "text/plain"));
+    }
+};
+
+int main(int argc, char** argv) {
+    webserver ws = create_webserver(8080);
+
+    digest_resource hwr;
+    ws.register_resource("/hello", &hwr);
+    ws.start(true);
+
+    return 0;
+}
---
+++ examples/minimal_deferred.cpp
@@ -0,0 +1,55 @@
+/*
+     This file is part of libhttpserver
+     Copyright (C) 2011, 2012, 2013, 2014, 2015 Sebastiano Merlino
+
+     This library is free software; you can redistribute it and/or
+     modify it under the terms of the GNU Lesser General Public
+     License as published by the Free Software Foundation; either
+     version 2.1 of the License, or (at your option) any later version.
+
+     This library is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+     Lesser General Public License for more details.
+
+     You should have received a copy of the GNU Lesser General Public
+     License along with this library; if not, write to the Free Software
+     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301
+     USA
+*/
+
+#include <httpserver.hpp>
+
+using namespace httpserver;
+
+static int counter = 0;
+
+ssize_t test_callback (char* buf, size_t max) {
+    if (counter == 2) {
+        return -1;
+    }
+    else {
+        memset(buf, 0, max);
+        strcat(buf, " test ");
+        counter++;
+        return std::string(buf).size();
+    }
+}
+
+class deferred_resource : public http_resource {
+    public:
+        const std::shared_ptr<http_response> render_GET(const http_request& req) {
+            return std::shared_ptr<deferred_response>(new deferred_response(test_callback, "cycle callback response"));
+        }
+};
+
+int main(int argc, char** argv) {
+    webserver ws = create_webserver(8080);
+
+    deferred_resource hwr;
+    ws.register_resource("/hello", &hwr);
+    ws.start(true);
+
+    return 0;
+}
+
