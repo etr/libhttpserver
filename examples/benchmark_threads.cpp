747ff7164def4c4259c98d3f29967ed5a0566770
Stuart Byma <stuart@observeinc.com>
2022-09-29 08:12:36 +0000

    revert accidental removal of const on pass by reference shared_ptrs

--- examples/benchmark_threads.cpp
+++ examples/benchmark_threads.cpp
@@ -28,7 +28,7 @@
 
 class hello_world_resource : public httpserver::http_resource {
  public:
-     explicit hello_world_resource(std::shared_ptr<httpserver::http_response>& resp):
+     explicit hello_world_resource(const std::shared_ptr<httpserver::http_response>& resp):
          resp(resp) {
      }
 
