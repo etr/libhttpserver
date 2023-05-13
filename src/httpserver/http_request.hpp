c66fb87bc9686771d7503f7ac4d4d7034aa6ee9e
bcsgh <33939446+bcsgh@users.noreply.github.com>
2023-05-13 16:17:13 -0700

    Move setting of http_request::cache out of the contructors.

--- src/httpserver/http_request.hpp
+++ src/httpserver/http_request.hpp
@@ -257,11 +257,11 @@ class http_request {
      /**
       * Default constructor of the class. It is a specific responsibility of apis to initialize this type of objects.
      **/
-     http_request() : cache(std::make_unique<http_request_data_cache>()) {}
+     http_request() = default;
 
      http_request(MHD_Connection* underlying_connection, unescaper_ptr unescaper):
          underlying_connection(underlying_connection),
-         unescaper(unescaper), cache(std::make_unique<http_request_data_cache>()) {}
+         unescaper(unescaper) {}
 
      /**
       * Copy constructor. Deleted to make class move-only. The class is move-only for several reasons:
@@ -411,7 +411,7 @@ class http_request {
         std::string digested_user;
         std::map<std::string, std::vector<std::string>, http::arg_comparator> unescaped_args;
      };
-     std::unique_ptr<http_request_data_cache> cache;
+     std::unique_ptr<http_request_data_cache> cache = std::make_unique<http_request_data_cache>();
      // Populate the data cache unescaped_args
      void populate_args() const;
 
