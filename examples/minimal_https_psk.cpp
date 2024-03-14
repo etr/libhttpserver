5b989770ad77ec8026b146f58c4b82306fd54619
oberdorc <96446443+oberdorc@users.noreply.github.com>
2024-03-14 17:06:31 +0100

    Update minimal_https_psk.cpp
    Fixing formatting issue highlighted by cpplint.

--- examples/minimal_https_psk.cpp
+++ examples/minimal_https_psk.cpp
@@ -20,7 +20,7 @@
 #include <httpserver.hpp>
 
 class hello_world_resource : public httpserver::http_resource {
-     public:
+ public:
     std::shared_ptr<httpserver::http_response> render(const httpserver::http_request&) {
         return std::shared_ptr<httpserver::http_response>(
             new httpserver::string_response("Hello, World!"));
